67ecd162365d28fef00f138ef5c6930eab94439f
kayoJr <kalebmelaku97@gmail.com>
2023-08-11 10:50:00 +0300

    project init and home page done

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,12 +7,12 @@ function Footer()
         <>
             <footer className="">
                 <div className="text max-w-[1240px] mx-auto">
-                    <ul className='text-white text-xl flex'>
-                        <li><Link className='text-xl border-0 border-r-2'>FACEBOOK</Link></li>
-                        <li><Link className='text-xl border-0 border-r-2'>TWITTER</Link></li>
-                        <li><Link className='text-xl border-0 border-r-2'>BEHANCE</Link></li>
-                        <li><Link className='text-xl border-0 border-r-2'>LINKEDIN</Link></li>
-                        <li><Link className='text-xl'>DRIBBBLE</Link></li>
+                    <ul className='text-white text-xl flex flex-col md:flex-row'>
+                        <li className='my-2 md:my-0'><Link className='text-xl border-0 border-none md:border-r-2'>TWITTER</Link></li>
+                        <li className='my-2 md:my-0'><Link className='text-xl border-0 border-none md:border-r-2'>FACEBOOK</Link></li>
+                        <li className='my-2 md:my-0'><Link className='text-xl border-0 border-none md:border-r-2'>BEHANCE</Link></li>
+                        <li className='my-2 md:my-0'><Link className='text-xl border-0 border-none md:border-r-2'>LINKEDIN</Link></li>
+                        <li className='my-2 md:my-0'><Link className='text-xl'>DRIBBBLE</Link></li>
                     </ul>
                 </div>
                 <p className='text-white text-center'>Copyright &copy; {year}</p>
