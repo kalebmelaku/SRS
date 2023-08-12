4f599562aefcb9aaf250d4378299a8a30663613a
kayoJr <kalebmelaku97@gmail.com>
2023-08-12 17:28:29 +0300

    first version

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ function Footer()
     return (
         <>
             <footer className="mt-8">
-                <div className="text max-w-[1240px] mx-auto">
+                <div className="text-sm max-w-[1240px] mx-auto">
                     <ul className='text-white flex flex-col md:flex-row'>
                         <li className='my-2 md:my-0'><Link className=' border-0 border-none md:border-r-2'>TWITTER</Link></li>
                         <li className='my-2 md:my-0'><Link className=' border-0 border-none md:border-r-2'>FACEBOOK</Link></li>
